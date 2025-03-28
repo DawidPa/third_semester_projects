e4783e89b044a9758055ed0614ba9c4912aa5a21
DawidPa <129805933+DawidPa@users.noreply.github.com>
2025-03-28 14:39:42 +0100

    Add files via upload

---
+++ arrays_string_operations/Zadanie9ProgramowanieSem3/Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie9ProgramowanieSem3
+{
+    //2.Tworzenie klasy i 2 atrybuty
+
+    class Home
+    {
+        public int attribute1;
+        public int attribute2;
+
+    }
+}
